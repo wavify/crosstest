c985cafe36e1da214170f8c9a5a37dd5bfd16771
Maythee Anegboonlap <null@llun.in.th>
2012-01-04 17:05:48 +0700

    Add TestTestRunner.

--- test/TestTestRunner.js
+++ test/TestTestRunner.js
@@ -4,13 +4,31 @@ var TestRunner = require('../lib/runner/TestRunner').TestRunner;
 
 TestIt ('TestTestRunner', {
   
-  'test run': function (test) {
-    var done = false;
+  'before each': function (test) {
+    var output = [];
     var object = {
-      
+      'before each': function (test) {
+        output.push('before each');
+      },
+      'test first': function (test) {
+        output.push('test first');
+      },
+      'test second': function (test) {
+        output.push('test second');
+      },
+      'test third': function (test) {
+        output.push('test third');
+      }
     };
     
-    var runner = new TestRunner('name', object);
+    var runner = new TestRunner('some name', object);
+    test.runner = runner;
+    test.output = output;
+  },
+  
+  'test run': function (test) {
+    var done = false;
+    var runner = test.runner;
     runner.run(function () {
       done = true;
     });
@@ -20,7 +38,40 @@ TestIt ('TestTestRunner', {
         return done || time > timeout;
       },
       function () {
+        var output = test.output;
+        test.assertEqual(6, output.length, 'Output should have 6 lines');
         
+        var expected = [ 'before each', 'test first',
+                         'before each', 'test second',
+                         'before each', 'test third'];
+        for (var index in expected) {
+          test.assertEqual(expected[index], output[index],
+            'Some output doesn\'t match expect output');
+        }
+                         
+      });
+  },
+  
+  'test run specific': function (test) {
+    var done = false;
+    var runner = test.runner;
+    runner.run('test first', function () {
+      done = true;
+    });
+    
+    test.waitFor(
+      function (time) {
+        return done || time > timeout;
+      },
+      function () {
+        var output = test.output;
+        test.assertEqual(2, output.length, 'Output should have 2 lines');
+        
+        var expected = [ 'before each', 'test first' ];
+        for (var index in expected) {
+          test.assertEqual(expected[index], output[index],
+            'Some output doesn\'t match expect output');
+        }
       });
   }
   
